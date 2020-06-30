b63a20deea57ce820b89a243bde9ef94beab78a0
Seregina508 <elenazav@mail.ru>
2020-06-30 18:26:25 -0400

    defaultFunctionality.js elements.js

--- test/defaultFunctionality.js
+++ test/defaultFunctionality.js
@@ -1,10 +1,12 @@
+const selectorCnt = require ('./../data/selectors.json').counter;
+
+
 describe('Default counter functionality', function () {
 
     describe('Calculation works', function () {
 
         it('TC-021 Subtract 1 gives -1', function () {
-            browser.url('https://likejean.github.io/homework-5/');
-            browser.pause(2000);
+            browser.url('');
             const button = $$('.btn-black')[0];
             button.click();
             const countValue = $('.badge').getText();
--- wdio.conf.js
+++ wdio.conf.js
@@ -17,7 +17,7 @@ exports.config = {
     // directory is where your package.json resides, so `wdio` will be called from there.
     //
     specs: [
-        './test/elements.js',
+        //'./test/elements.js',
         './test/defaultfunctionality.js'
     ],
     // Patterns to exclude.
@@ -66,7 +66,7 @@ exports.config = {
     // Define all options that are relevant for the WebdriverIO instance here
     //
     // Level of logging verbosity: trace | debug | info | warn | error | silent
-    logLevel: 'info',
+    logLevel: 'silent',//'info',
     //
     // Set specific log levels per logger
     // loggers:
